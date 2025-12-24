d956605c5f5806876507e962c7ac2f358db45cb7
Rahul Choudhary <rahulchoudhary0rj18@gmail.com>
2025-12-24 21:01:17 +0530

    Delete DEMO directory

--- DEMO/locustfile.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task, between
-
-class WeatherUser(HttpUser):
-    # Wait between 1 and 3 seconds between searches (realistic behavior)
-    wait_time = between(1, 3)
-
-    @task
-    def get_weather(self):
-        self.client.get("/weather/Jaipur")
